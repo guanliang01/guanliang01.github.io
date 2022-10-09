899cc9213aa0bef74688b7bfd009af1550ff2caf
guanliang01 <176487789@163.com>
2022-10-09 17:59:05 +0800

    111
    111111

---
+++ js/js.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-11-03 10:04:22
+ * @version $Id$
+ */
+
+$(function(){
+	$('.nav-rig').click(function(event) {
+		$('.nav-rig ul').toggle();
+		
+	});
+});
